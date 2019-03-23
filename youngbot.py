37e60799770b78421fc14660b42ecf9088d7cc05
hchozzar <48625607+hchozzar@users.noreply.github.com>
2019-03-23 12:27:56 +0900

    Add files via upload

---
+++ youngbot.py
@@ -0,0 +1,28 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!창휘'):
+        await client.send_message(message.channel, '만나서 반가워요 영재')
+
+    elif message.content.startswith('!성준'):
+        await client.send_message(message.channel, '영재같은 놈')
+        msg = await client.wait_for_message(timeout=15.0, author=message.author)
+
+        if msg is None:
+            await client.send_message(message.channel, '15초내로 입력해주세요. 다시시도: !say')
+            return
+        else:
+            await client.send_message(message.channel, msg.content)
+
+client.run('NTU4NTQxMzMzMzI1Njc2NTQ4.D3Z-iA.mJTMoxrVZM171JD60Dy7fGR_qd4')
